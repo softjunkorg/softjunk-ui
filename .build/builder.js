20fb3e4fe1142b84033dd8c8ee0ba8333907eb5e
felpsdev <felipecp2007@gmail.com>
2022-07-31 19:28:20 -0300

    fix(code_of_conduct) contact

--- .build/builder.js
+++ .build/builder.js
@@ -1,31 +1,45 @@
-const { dtsPlugin } = require("esbuild-plugin-d.ts");
+const { dts } = require("esbuild-plugin-d.ts");
 const { build } = require("esbuild");
 const colors = require("colors");
-const config = require("../builder.json");
+const config = require("../package.json");
 
-const isWatch = process.argv.findIndex((argItem) => argItem === "--watch") >= 0;
+const isWatch = process.argv.findIndex(argItem => argItem === "--watch") >= 0;
+
+if (!config.builderSettings) {
+    throw new Error("Builder: No settings were found on the package.json!");
+}
+
+function runBuild(config) {
+    build({
+        ...config,
+        plugins: [dts()],
+        watch: isWatch
+            ? {
+                  onRebuild: function (err, res) {
+                      if (err) {
+                          return console.error(
+                              `[${colors.red("BUILDER")}]: Error on listening`,
+                              err,
+                          );
+                      }
+
+                      console.log(
+                          `[${colors.green("BUILDER")}]: Listened to changes.`,
+                      );
+                  },
+              }
+            : false,
+    });
+}
 
 if (isWatch) {
     console.log(`[${colors.green("BUILDER")}]: Started listening to changes.`);
 }
 
-build({
-    ...config,
-    plugins: [dtsPlugin()],
-    watch: isWatch
-        ? {
-              onRebuild: (err, res) => {
-                  if (err) {
-                      return console.error(
-                          `[${colors.red("BUILDER")}]: Error on listening`,
-                          err
-                      );
-                  }
-
-                  console.log(
-                      `[${colors.green("BUILDER")}]: Listened to changes.`
-                  );
-              },
-          }
-        : false,
-});
+if (Array.isArray(config.builderSettings.options)) {
+    config.builderSettings.options.map(function (c) {
+        runBuild(c);
+    });
+} else {
+    runBuild(config.builderSettings.options);
+}
